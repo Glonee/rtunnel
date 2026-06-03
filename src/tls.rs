4f6e0abfce157faa3d60ce5078c344e8b9646383
Glonee <glonee@foxmail.com>
2026-06-03 10:57:31 +0800

    Tune TLS client fingerprint closer to Chrome

--- src/tls.rs
+++ src/tls.rs
@@ -2,18 +2,42 @@ use boring::ssl::{SslConnector, SslFiletype, SslMethod, SslOptions, SslVerifyMod
 
 use crate::config::TlsServerConfig;
 
+const CHROME_TLS12_CIPHERS: &str = "\
+    ECDHE-ECDSA-AES128-GCM-SHA256:\
+    ECDHE-RSA-AES128-GCM-SHA256:\
+    ECDHE-ECDSA-AES256-GCM-SHA384:\
+    ECDHE-RSA-AES256-GCM-SHA384:\
+    ECDHE-ECDSA-CHACHA20-POLY1305:\
+    ECDHE-RSA-CHACHA20-POLY1305:\
+    ECDHE-RSA-AES128-SHA:\
+    ECDHE-RSA-AES256-SHA:\
+    AES128-GCM-SHA256:\
+    AES256-GCM-SHA384:\
+    AES128-SHA:\
+    AES256-SHA";
+
+const CHROME_SIGNATURE_ALGORITHMS: &str = "\
+    ecdsa_secp256r1_sha256:\
+    rsa_pss_rsae_sha256:\
+    rsa_pkcs1_sha256:\
+    ecdsa_secp384r1_sha384:\
+    rsa_pss_rsae_sha384:\
+    rsa_pkcs1_sha384:\
+    rsa_pss_rsae_sha512:\
+    rsa_pkcs1_sha512";
+
 pub fn chrome_like_connector(insecure: bool) -> anyhow::Result<SslConnector> {
     let mut builder = SslConnector::builder(SslMethod::tls_client())?;
     builder.set_min_proto_version(Some(SslVersion::TLS1_2))?;
     builder.set_max_proto_version(Some(SslVersion::TLS1_3))?;
     builder.set_alpn_protos(b"\x02h2\x08http/1.1")?;
-    builder.set_cipher_list(
-        "TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:\
-         ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:\
-         ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:\
-         ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305",
-    )?;
+    builder.set_cipher_list(CHROME_TLS12_CIPHERS)?;
     builder.set_curves_list("X25519:P-256:P-384")?;
+    builder.set_sigalgs_list(CHROME_SIGNATURE_ALGORITHMS)?;
+    builder.set_grease_enabled(true);
+    builder.set_permute_extensions(true);
+    builder.enable_ocsp_stapling();
+    builder.enable_signed_cert_timestamps();
     builder.set_options(SslOptions::NO_COMPRESSION);
     if insecure {
         builder.set_verify(SslVerifyMode::NONE);
